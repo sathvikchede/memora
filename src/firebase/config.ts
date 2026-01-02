a3dd457f79abb4c6c020032422be94a2334df08a
Sathvik Chede <sathvikchede@gmail.com>
2026-01-02 12:58:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7680548045-10799",
+  "appId": "1:843069908844:web:e7061df10c2ae077be3549",
+  "apiKey": "AIzaSyDLaXRLvLvhcqGIkXMNRTZAbRhwLWyoFRQ",
+  "authDomain": "studio-7680548045-10799.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "843069908844"
+};
